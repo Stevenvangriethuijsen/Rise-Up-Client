93112d5d6820a772fa0465fda76a8627d9765743
Stevenvangriethuijsen <Stevenvangriethuijsen@Outlook.com>
2020-04-12 16:36:38 +0200

    cleaned up basic react app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
-
+import { Route } from "react-router-dom";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello world</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
